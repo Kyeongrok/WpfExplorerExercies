40288d73763adc36566fec99fb2d10b9e2f5302a
kyeongrok.kim <kyeongrok.kim@e3view.com>
2025-12-21 16:08:43 +0900

    App.cs, Starter.cs

---
+++ WpfExplorer/App.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+using Jamesnet.Wpf.Controls;
+
+namespace WpfExplorer3;
+
+internal class App : JamesApplication
+{
+    protected override Window CreateShell()
+    {
+        return new Window();
+    }
+}
---
+++ WpfExplorer/Starter.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfExplorer3;
+
+internal class Starter
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        _ = new App().Run();
+    }
+}
